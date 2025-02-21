8b038d2cd1bc6bf980486aeccbbed81b56cbc37e
seb-navarro <navarrosebastian154@gmail.com>
2025-02-21 18:02:33 +0000

    changed player back to character body 2d node

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,10 +1,10 @@
-extends Area2D
+extends CharacterBody2D
 
 signal hit
 
-@export var speed = 300
 var screen_size
-
+var speed = 100
+var gravity = 5000
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,31 +14,44 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var velocity = Vector2.ZERO
+	
+	velocity.y = gravity * delta
+	
+	var direction = 0
 	
 	if Input.is_action_pressed("move_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1
+		direction += 1
+	elif Input.is_action_pressed("move_left"):
+		direction -= 1
 	
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
+	velocity.x = direction * speed
+	
+	move_and_slide()
+	
+	check_collision()
 	
 	$AnimatedSprite2D.play()
 	
 	position += velocity * delta
-
+	
 	var player_size = Vector2(64, 64)
+	
 	position.x = clamp(position.x, 0 + player_size.x / 2, screen_size.x - player_size.x / 2)
+	#position.y = clamp(position.x, 0 + player_size.y / 2, screen_size.y - player_size.y / 2)
+	#position = position.clamp(Vector2.ZERO, screen_size)
 	
 	if velocity.x != 0:
 		$AnimatedSprite2D.flip_h = velocity.x < 0
 
 
-func _on_body_entered(body: Node2D) -> void:
-	hit.emit()
-	$AnimatedSprite2D.animation = "submarine_hit"
-	$HitTimer.start()
+
+func check_collision():
+	if get_slide_collision_count() > 0:
+		hit.emit()
+		$AnimatedSprite2D.animation = "submarine_hit"
+		$CollisionPolygon2D.disabled = true
+		$HitTimer.start()
+
 
 
 func start(pos):
@@ -49,3 +62,4 @@ func start(pos):
 
 func _on_hit_timer_timeout() -> void:
 	$AnimatedSprite2D.animation = "submarine"
+	$CollisionPolygon2D.disabled = false
