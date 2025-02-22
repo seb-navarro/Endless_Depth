0f799d0989c480ba6973cfd109e20b81b2629fad
seb-navarro <navarrosebastian154@gmail.com>
2025-02-22 20:08:03 +0000

    added camera and made enemy spawn follow camera
    Potential to make "Camera2D" its own  separate scene.

---
+++ Camera/camera_2d.gd
@@ -0,0 +1,34 @@
+extends Camera2D
+
+@export var enemy_scene: PackedScene
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	#$EnemyPath/EnemySpawnLocation.offset += velocity * delta / path.length
+	$EnemyPath/EnemySpawnLocation.position += position
+
+
+
+func _on_enemy_timer_timeout() -> void:
+	var enemy = enemy_scene.instantiate()
+	
+	var enemy_spawn_location = $EnemyPath/EnemySpawnLocation
+	enemy_spawn_location.progress_ratio = randf()
+
+	var direction = enemy_spawn_location.rotation + PI / 2
+
+	enemy.position = enemy_spawn_location.position
+
+	direction += randf_range(-PI / 4, PI / 4)
+	enemy.rotation = direction
+
+	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
+	enemy.linear_velocity = velocity.rotated(direction)
+
+	add_child(enemy)
--- Main/main.gd
+++ Main/main.gd
@@ -1,6 +1,5 @@
 extends Node
 
-@export var enemy_scene: PackedScene
 var fuel = 100
 var depth = 0
 
@@ -26,37 +25,17 @@ func new_game():
 	$StartTimer.start()
 
 
-func _on_enemy_timer_timeout() -> void:
-	var enemy = enemy_scene.instantiate()
-	
-	var enemy_spawn_location = $EnemyPath/EnemySpawnLocation
-	enemy_spawn_location.progress_ratio = randf()
-
-	var direction = enemy_spawn_location.rotation + PI / 2
-
-	enemy.position = enemy_spawn_location.position
-
-	direction += randf_range(-PI / 4, PI / 4)
-	enemy.rotation = direction
-
-	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
-	enemy.linear_velocity = velocity.rotated(direction)
-
-	add_child(enemy)
-
-
-
 func _on_fuel_deplete_timeout() -> void:
 	fuel -= 1
 
 
 func _on_start_timer_timeout() -> void:
-	$EnemyTimer.start()
+	$Player/Camera2D/EnemyTimer.start()
 	$FuelDeplete.start()
 	$DepthTimer.start()
 
 func game_over():
-	$EnemyTimer.stop()
+	$Player/Camera2D/EnemyTimer.stop()
 	$FuelDeplete.stop()
 	$DepthTimer.stop()
 	$HUD.show_game_over()
