8e70e1967a7bf01d420a14d1812ab8cf78b91506
seb-navarro <navarrosebastian154@gmail.com>
2025-02-23 11:56:31 +0000

    fixed fish spawning out of the game area glitch

--- Camera/camera_2d.gd
+++ Camera/camera_2d.gd
@@ -3,9 +3,10 @@ extends Camera2D
 @export var enemy_scene: PackedScene
 
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
