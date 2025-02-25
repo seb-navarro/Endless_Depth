2a28f0aee504a04cab7621ef9501c1a029ab2216
seb-navarro <navarrosebastian154@gmail.com>
2025-02-25 17:56:19 +0000

    fixed start button glitch

--- Menu/menu.gd
+++ Menu/menu.gd
@@ -1,16 +1,25 @@
 extends Control
 
+var pressed
+var movement = 100
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$Subamrine.play("subamrine_menu")
+	pressed = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if pressed == true:
+		$Subamrine.position.y += movement * delta
+		$Subamrine.speed_scale = 10
 
 
-
-
-func _on_start_button_pressed() -> void:
+func _on_menu_timer_timeout() -> void:
 	get_tree().change_scene_to_file("res://Main/Main.tscn")
+
+
+func _on_start_button_released() -> void:
+	$MenuTimer.start()
+	pressed = true
+	$StartButton.visible = false
