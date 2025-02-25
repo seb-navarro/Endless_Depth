d2e6774f270704d3f88241ac3a4fcd35b531dd76
seb-navarro <navarrosebastian154@gmail.com>
2025-02-25 14:50:32 +0000

    added a main menu screen

--- Menu/menu.gd
+++ Menu/menu.gd
@@ -9,3 +9,9 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Main/Main.tscn")
